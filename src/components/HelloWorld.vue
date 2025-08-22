90537bff2c9256055dabdfb47eaa902e3506c13e
Manami <manami@bake250.com>
2025-08-22 18:39:52 +0200

    Migrate to Vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <button type="button" @click="count++">{{ msg }}count is {{ count }}</button>
+
+
+</template>
+
